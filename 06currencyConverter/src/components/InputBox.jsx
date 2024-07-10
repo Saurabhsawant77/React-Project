7a3fc2f8319a58f893f9df619ca0c2fd786199ba
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-07-10 23:37:45 +0530

    09Theme Switcher

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -41,7 +41,7 @@ function InputBox({
                 >
                     
                         {currencyOptions.map((currency) => (
-                            <option key={currency} value={currency}>
+                        <option key={currency} value={currency}>
                             {currency}
                         </option>
                         ))}
